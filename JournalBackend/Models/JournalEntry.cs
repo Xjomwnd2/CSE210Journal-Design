e60e21b2c2159d384ce0ac36b57a9c1f05b5438b
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-15 00:33:59 +0300

    commit

---
+++ JournalBackend/Models/JournalEntry.cs
@@ -0,0 +1,10 @@
+namespace JournalBackend.Models
+{
+    public class JournalEntry
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    }
+}
