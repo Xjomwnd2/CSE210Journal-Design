b9ea7ce8d7e7fc3396fdc5b2169efd3a99cdb4ae
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-15 00:46:29 +0300

    commit

--- JournalApp/JournalBackend/obj/Debug/net10.0/JournalBackend.AssemblyInfo.cs
+++ JournalApp/JournalBackend/obj/Debug/net10.0/JournalBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e60e21b2c2159d384ce0ac36b57a9c1f05b5438b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e3390cb59a35bb7b2fbe9040f2da686ebb4a25d")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
