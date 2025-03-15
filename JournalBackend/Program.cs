f0ec86e303415335ecbecf36bdaca16e3f78a9db
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-15 19:21:58 +0000

    Mar 15, 2025, 10:21 PM

--- JournalBackend/Program.cs
+++ JournalBackend/Program.cs
@@ -59,3 +59,26 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+// ADD
+using JournalBackend.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddDbContext<JournalDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
