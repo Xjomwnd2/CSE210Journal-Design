efcd3e031cc6f91e3d0eb4ea1b20ef72ead514b3
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-15 20:11:38 +0000

    Mar 15, 2025, 11:11 PM

--- JournalApp/JournalBackend/obj/Debug/net8.0/JournalBackend.AssemblyInfo.cs
+++ JournalApp/JournalBackend/obj/Debug/net8.0/JournalBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c887f57a581a12881e0ae46af04bee2c3cdbaee9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8383543f40c15324d8a2638f3bfaf799c60bc94")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
