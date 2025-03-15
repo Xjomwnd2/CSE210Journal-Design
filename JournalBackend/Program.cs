7d1fa82891fa21af148a7576e0f8699dec2caea1
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-15 23:17:02 +0300

    commit

--- JournalBackend/Program.cs
+++ JournalBackend/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
-using JournalBackend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
